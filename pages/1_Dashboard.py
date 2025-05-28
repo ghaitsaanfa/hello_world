dd31a9d6beb766cfb15796d0a65c15f4a7af2355
ghaitsaanfa <ghaitsaanfa05@students.unnes.ac.id>
2025-05-28 14:19:26 +0700

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -1,1 +1,8 @@
+import streamlit as st
+
+st.title("Dashboard")
+st.subheader("Ini adalah tampilan Tes di halaman Dashboard")
+
+# Konten Tes
+st.write("Tes berhasil ditampilkan di halaman Dashboard.")
 
